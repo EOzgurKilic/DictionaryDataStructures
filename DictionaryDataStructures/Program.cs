9e93e952f908099e3427f05be5a7791dec59ee37
orkhaner <orkhanrrahimli@gmail.com>
2025-06-25 20:01:20 +0300

    Some questions revised.

--- DictionaryDataStructures/Program.cs
+++ DictionaryDataStructures/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Drawing;
+using System.Text;
 
 namespace DictionaryDataStructures;
 
@@ -11,24 +12,23 @@ class Program
         string str2 = "rat";
         Console.WriteLine(IsAnagram(str1, str2));*/
 
-
         //TwoSum
         /*int[] arr = new int[3] { 3, 3, 3 };
         var pair = TwoSum(arr, 6);
         Console.WriteLine(pair[0] + " " +pair[1]);*/
-        
-        
+
+
         //LongestCommonPrefix
         /*string[] strs = {"abc", "abcd", "abcde", "abcdef"};
         string pref = LongestCommonPrefix(strs);*/
-        
-        
-        LinkedList<int> list1 = new LinkedList<int>();
+
+
+        /*LinkedList<int> list1 = new LinkedList<int>();
         list1.AddFirst(4);
         list1.AddFirst(3);
         list1.AddFirst(2);
         list1.AddFirst(1);
-        
+
         LinkedList<int> list2 = new LinkedList<int>();
         LinkedListNode<int> node1 = list1.First;
 
@@ -39,8 +39,34 @@ class Program
         }
 
         foreach (var VARIABLE in list2)
-            Console.WriteLine(VARIABLE);
-        
+            Console.WriteLine(VARIABLE);*/
+
+
+        //MyHashMap
+        MyHashMap myHashMap = new MyHashMap();
+        myHashMap.Put(1, 1);
+        myHashMap.Put(2, 2);
+        Console.WriteLine(myHashMap.Get(1));
+        myHashMap.Get(3);
+        myHashMap.Put(2, 1);
+        myHashMap.Put(3, 1);
+        myHashMap.Put(4, 1);
+        myHashMap.Put(5, 1);
+        myHashMap.Put(6, 1);
+        myHashMap.Put(7, 1);
+        myHashMap.Get(2);
+        myHashMap.Remove(2);
+        myHashMap.Get(2);
+
+        int[] arr1 = new int[10000], _bucket = new int[16], _temp = new int[16];
+
+    arr1[1] = 5;
+        _temp = (int[])_bucket.Clone();
+        _bucket = new int[2*_bucket.Length];
+        _temp.CopyTo(_bucket,0);
+        Console.WriteLine(2*_bucket.Length);
+
+
     }
     public static bool IsAnagram(string s, string t) //https://leetcode.com/problems/valid-anagram/
     {
@@ -49,21 +75,15 @@ class Program
         
         Dictionary<char, int> dict = new Dictionary<char, int>();
         foreach (var str in s)
-        {
             if (!dict.ContainsKey(str))
                 dict.Add(str, 1);
             else
                 dict[str]++;
-        }
-
-        foreach (var str in t)
-        {
-            if (!dict.ContainsKey(str))
+        
+        foreach (char k in t)
+            if (!dict.ContainsKey(k) || --dict[k] < 0)
                 return false;
-            else if (--dict[str] < 0)
-                return false;
-        }
-
+    
         return true;
     }
     public static int[] TwoSum(int[] nums, int target) //https://leetcode.com/problems/two-sum/
@@ -144,5 +164,45 @@ class MyDictionary<Tkey, Tvalue>
         }
         return false;
     }
-    
 }
+
+public class MyHashMap //https://leetcode.com/problems/design-hashmap/
+{
+    int[] _bucket, _temp;
+
+    public MyHashMap() {
+        _bucket = new int[16];
+    }
+    
+    public void Put(int key, int value) {
+        if(key >= _bucket.Length)
+            Resize(key);
+
+        _bucket[key] = value + 1;
+    }
+    
+    public int Get(int key)
+    {
+        if (key >= _bucket.Length)
+            return - 1;
+        return _bucket[key] - 1;
+    }
+    
+    public void Remove(int key) {
+        if (key >= _bucket.Length)
+            return;
+        _bucket[key] = 0;
+    }
+
+    public void Resize(int key)
+    {
+        if (key < _bucket.Length*2)
+            Resize(key);
+        _temp = (int[])_bucket.Clone();
+        _bucket = new int[2*_bucket.Length];
+        _temp.CopyTo(_bucket,0);
+    }
+}
+
+
+
